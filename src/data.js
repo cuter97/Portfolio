1b9b63402e25d677228b37d6e80e2c0a4c4c9f57
cuter97 <aguscastets@gmail.com>
2022-04-26 16:24:49 -0300

    add info

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const data = [
     {
         id: 1,
         tittle: "Calculator",
-        description: "This project was a front end mentor challenge. The challenge is to integrate the rel.ink API into a web user interface to create shortened URLs. They should be displayed according to the proposed design. I took this chance to get some more experience with asynchronous JavaScript.",
+        description: "This project was a frontend mentor challenge. The challenge was to create a calculator with a toggle that applies different themes, incorporating localstorage so that the last theme you used is saved.",
         tags: ["CSS3", "HTML", "React"],
         tools: ["Sass", "Styled components"],
         github: "https://github.com/cuter97/Calculadora",
@@ -11,7 +11,7 @@ export const data = [
     {
         id: 2,
         tittle: "Shortly Api",
-        description: "This project was a front end mentor challenge. The challenge is to integrate the rel.ink API into a web user interface to create shortened URLs. They should be displayed according to the proposed design. I took this chance to get some more experience with asynchronous JavaScript.",
+        description: "This project was a frontend mentor challenge. The challenge is to integrate the rel.ink API into a web user interface to create shortened URLs. They should be displayed according to the proposed design. I took this chance to get some more experience with asynchronous JavaScript.",
         tags: ["CSS3", "HTML", "React", "API"],
         tools: ["Sass"],
         github: "https://github.com/cuter97/shortly-api",
@@ -20,7 +20,7 @@ export const data = [
     {
         id: 3,
         tittle: "HOPS Beer",
-        description: "This project was a front end mentor challenge. The challenge is to integrate the rel.ink API into a web user interface to create shortened URLs. They should be displayed according to the proposed design. I took this chance to get some more experience with asynchronous JavaScript.",
+        description: "This project was a challenge for the coderhouse platform where I learned a lot from react and incorporated firebase as a backend from where I get the information and simulate a possible purchase.",
         tags: ["CSS3", "HTML", "React"],
         tools: ["Sass", "Bootstrap", "Firebase"],
         github: "https://github.com/cuter97/React.js",
@@ -29,7 +29,7 @@ export const data = [
     {
         id: 4,
         tittle: "JS Project",
-        description: "This project was a front end mentor challenge. The challenge is to integrate the rel.ink API into a web user interface to create shortened URLs. They should be displayed according to the proposed design. I took this chance to get some more experience with asynchronous JavaScript.",
+        description: "This project was for a coderhouse challenge where I studied web development. This project was to practice vanilla javascript using jquery for animations and a public API to get the information from.",
         tags: ["CSS3", "HTML", "JavaScript", "API"],
         tools: ["Sass", "Bootstrap", "JQuery"],
         github: "https://github.com/cuter97/Proyecto",
@@ -38,7 +38,7 @@ export const data = [
     {
         id: 5,
         tittle: "Bariloche Pubs",
-        description: "This project was a front end mentor challenge. The challenge is to integrate the rel.ink API into a web user interface to create shortened URLs. They should be displayed according to the proposed design. I took this chance to get some more experience with asynchronous JavaScript.",
+        description: "Cervezas Bariloche was my first project to show the different breweries that exist in the region where I live and learn a little about their history. This project was made only with html and css, incorporating tools such as sass and Bootstrap to adapt it to mobile.",
         tags: ["CSS3", "HTML"],
         tools: ["Sass", "Bootstrap"],
         github: "https://github.com/cuter97/Cervezas",
@@ -47,7 +47,7 @@ export const data = [
     {
         id: 6,
         tittle: "Portfolio",
-        description: "This project was a front end mentor challenge. The challenge is to integrate the rel.ink API into a web user interface to create shortened URLs. They should be displayed according to the proposed design. I took this chance to get some more experience with asynchronous JavaScript.",
+        description: "I made this portfolio with the idea of showing some of my personal projects and adding more works as time goes by. This project is adapted to mobile and incorporates tools such as framer motion for cards and styled components.",
         tags: ["CSS3", "HTML", "React"],
         tools: ["Sass", "Styled components", "Framer motion"],
         github: "https://github.com/cuter97/Portfolio",
